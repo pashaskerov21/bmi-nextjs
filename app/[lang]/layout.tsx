67483a69380f3473d2c4b9fce7f0672878fbd055
pasha <askerovpasha21@gmail.com>
2023-11-12 01:30:09 +0400

    hide RootLayout

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -8,31 +8,40 @@ export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }))
 }
 
-// export const metadata = {
-//   title: 'BMI',
-//   description: 'Business Management Institute',
-// }
+export const metadata = {
+  title: 'BMI',
+  description: 'Business Management Institute',
+}
 
 export default async function Root({ children, params, }: { children: React.ReactNode, params: { lang: string } }) {
-  const [settingData, settingTranslateData, menuData, menuTranslateData]:
-    [SettingType[] | undefined, SettingTranslateType[] | undefined, MenuType[] | undefined, MenuTranslateType[] | undefined] = await Promise.all([
-      fetchSetting(), fetchSettingTranslate(), fetchMenu(), fetchMenuTranslate()])
-  if (settingData && settingTranslateData && menuData && menuTranslateData) {
-    const requiredSettingTranslate: SettingTranslateType | undefined = settingTranslateData.find((data) => data.lang === params.lang && data.setting_id === settingData[0].id);
-    const requiredMenuTranslate: MenuTranslateType[] | undefined = menuTranslateData.filter((data) => data.lang === params.lang)
-    if (requiredSettingTranslate && requiredMenuTranslate) {
-      return (
-        <html lang={params.lang}>
-          <head>
-            <RootHead settingData={settingData[0]} requiredSettingTranslate={requiredSettingTranslate} />
-          </head>
-          <StyledComponentsRegistry>
-            <RootLayout settingData={settingData[0]} menuData={menuData} requiredMenuTranslate={requiredMenuTranslate} currentLanguage={params.lang}>
-              {children}
-            </RootLayout>
-          </StyledComponentsRegistry>
-        </html>
-      )
-    }
-  }
+  <html lang={params.lang}>
+    <body>
+      {children}
+    </body>
+  </html>
 }
+
+
+// export default async function Root({ children, params, }: { children: React.ReactNode, params: { lang: string } }) {
+//   const [settingData, settingTranslateData, menuData, menuTranslateData]:
+//     [SettingType[] | undefined, SettingTranslateType[] | undefined, MenuType[] | undefined, MenuTranslateType[] | undefined] = await Promise.all([
+//       fetchSetting(), fetchSettingTranslate(), fetchMenu(), fetchMenuTranslate()])
+//   if (settingData && settingTranslateData && menuData && menuTranslateData) {
+//     const requiredSettingTranslate: SettingTranslateType | undefined = settingTranslateData.find((data) => data.lang === params.lang && data.setting_id === settingData[0].id);
+//     const requiredMenuTranslate: MenuTranslateType[] | undefined = menuTranslateData.filter((data) => data.lang === params.lang)
+//     if (requiredSettingTranslate && requiredMenuTranslate) {
+//       return (
+//         <html lang={params.lang}>
+//           <head>
+//             <RootHead settingData={settingData[0]} requiredSettingTranslate={requiredSettingTranslate} />
+//           </head>
+//           <StyledComponentsRegistry>
+//             <RootLayout settingData={settingData[0]} menuData={menuData} requiredMenuTranslate={requiredMenuTranslate} currentLanguage={params.lang}>
+//               {children}
+//             </RootLayout>
+//           </StyledComponentsRegistry>
+//         </html>
+//       )
+//     }
+//   }
+// }
